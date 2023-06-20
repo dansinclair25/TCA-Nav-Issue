da378b255cd8578902cd279eb3d4fe1f2256826d
Dan Sinclair <dansinclair@me.com>
2023-06-20 20:23:49 +0100

    Initial Commit

---
+++ tca-issue/tca_issueApp.swift
@@ -0,0 +1,17 @@
+//
+//  tca_issueApp.swift
+//  tca-issue
+//
+//  Created by Dan Sinclair on 20/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct tca_issueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
